4453dafbc0762fba9b5a4cca9387f9bfc3d344bb
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-08-17 23:17:47 +0530

    fixed prefix

--- backend/app/user/routes.py
+++ backend/app/user/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,HTTPException,Depends
 from user.db import get_user_access_token
 import httpx
 
-router = APIRouter(prefix="user",tags=["user"])
+router = APIRouter(prefix="/user",tags=["user"])
 
 @router.get("/repos")
 async def get_user_repos(access_token:str = Depends(get_user_access_token)):
